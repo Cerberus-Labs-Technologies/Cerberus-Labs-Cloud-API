a519fd3678fb296bacc37ec241a9c3c80da4d1a5
NecrossIT <kelvin.bill@necross.it>
2023-10-23 11:27:39 +0200

    feature: report module

---
+++ network/rest/modules/reports/model.go
@@ -0,0 +1,15 @@
+package reports
+
+import "github.com/Cerberus-Labs-Technologies/Cerberus-Labs-Cloud-API/database"
+
+type Report struct {
+	ID              int                 `json:"id" db:"id"`
+	ReportingPlayer database.JsonString `json:"reportingPlayer" db:"reporting_player"`
+	ReportedPlayer  database.JsonString `json:"reportedPlayer" db:"reported_player"`
+	Reason          database.JsonString `json:"reason" db:"reason"`
+	Investigated    database.IntBool    `json:"investigated" db:"investigated"`
+	InvestigatedBy  database.JsonString `json:"investigatedBy" db:"investigated_by"`
+	CreatedAt       database.TimeStamp  `json:"createdAt" db:"created_at"`
+	UpdatedAt       database.TimeStamp  `json:"updatedAt" db:"updated_at"`
+	InvestigatedAt  database.TimeStamp  `json:"investigatedAt" db:"investigated_at"`
+}
---
+++ network/rest/modules/reports/packets.go
@@ -0,0 +1,27 @@
+package reports
+
+type PlayerReportedPacket struct {
+	Name   string `json:"name"`
+	Report Report `json:"report"`
+}
+
+type ReportInvestigatedPacket struct {
+	Name   string `json:"name"`
+	Report Report `json:"report"`
+}
+
+type ReportUpdatedPacket struct {
+	Name      string `json:"name"`
+	OldReport Report `json:"oldReport"`
+	NewReport Report `json:"newReport"`
+}
+
+type ReportDeletedPacket struct {
+	Name   string `json:"name"`
+	Report Report `json:"report"`
+}
+
+type ReportCreatedPacket struct {
+	Name   string `json:"name"`
+	Report Report `json:"report"`
+}
